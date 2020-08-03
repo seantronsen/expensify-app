0deb223085d9c4ff885cc9193dc9f7939b7b622c
seantronsen <sean.tronsen@gmail.com>
2020-08-03 03:41:03 -0400

    update and push to prep for the hurricane

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,40 +1,118 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyAGGhIrR1zf9IrHEIdm4KGZrSpzRqL2GPQ",
-  authDomain: "expensify-5776f.firebaseapp.com",
-  databaseURL: "https://expensify-5776f.firebaseio.com",
-  projectId: "expensify-5776f",
-  storageBucket: "expensify-5776f.appspot.com",
-  messagingSenderId: "893684908013",
-  appId: "1:893684908013:web:74cd48d75ed8c96c9771e4",
-  measurementId: "G-S7TLEM6NLE",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 
 const database = firebase.database();
-database
-  .ref()
-  .set({
-    name: "Sean Tronsen",
-    age: 20,
-    isSingle: true,
-    stressLevel: 4,
-    job: {
-      title: "Software Developer",
-      company: "nvidia",
-    },
-    location: {
-      city: "Seattle",
-      country: "United States",
-    },
-  })
-  .then((data) => {
-    console.log("data is saved");
-  })
-  .catch((e) => {
-    console.log("Failed: ", e);
-  });
+
+export { firebase, database as default };
+
+// database.ref("expenses").on("child_removed", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref("expenses").on("child_changed", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref("expenses").on("child_added", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// Setup "expenses" with three items containing the properties (description, note, amount, createdAt)
+
+// const onValueChange = database.ref("expenses").on("value", (snapshot) => {
+//   const expenses = [];
+//   snapshot.forEach((item) => {
+//     expenses.push({ ...item.val(), id: item.key });
+//   });
+//   console.log(expenses);
+// });
+
+// setTimeout(undefined, 2000);
+// for (var x = 0; x < 3; x++)
+//   database.ref("expenses").push({
+//     description: "Bill for failed hernia surgery",
+//     note: "why",
+//     amount: Math.ceil(Math.random() * 10000 * x * x * x * x),
+//     createdAt: x,
+//   });
+// database.ref("notes").set(null);
+// database.ref("notes").push({ body: "this is another note", title: "Test note" });
+// database.ref("notes").remove("-MDIGVBwOkVVAngfRzCy");
+
+// const firebaseNotes = {
+//   notes: {
+//     apoijasdf: {
+//       body: "this is the note",
+//       title: "Test note",
+//     },
+//     apasdfasdf: {
+//       body: "this is another note",
+//       title: "Test note",
+//     },
+//   },
+// };
+
+// const notes = [
+//   { id: "12", body: "this is the note", title: "Test note" },
+//   { id: "14", body: "this is another note", title: "Test note" },
+// ];
+
+// database.ref("notes").set(notes);
+
+// Setup a new data subscription -> Sean is a software developer at Amazon
+// Change the data and make sure that the result of the change is output to the console.
+
+// const onValueChange = database.ref().on("value", (snapshot) => {
+//   const value = snapshot.val();
+//   console.log(`${value.name} is a ${value.job.title} at ${value.job.company}.`);
+// });
+
+// database.ref("job").update({
+//   title: "Computer Engineer",
+//   company: "Nvidia",
+// });
+
+// database
+//   .ref()
+//   .once("value")
+//   .then((data) => {
+//     console.log("Data: ", data.val());
+//   })
+//   .catch((error) => {
+//     console.log("Error: ", error);
+//   });
+
+// database
+//   .ref()
+//   .set({
+//     name: "Sean Tronsen",
+//     age: 20,
+//     isSingle: true,
+//     stressLevel: 4,
+//     job: {
+//       title: "Software Developer",
+//       company: "nvidia",
+//     },
+//     location: {
+//       city: "Seattle",
+//       country: "United States",
+//     },
+//   })
+//   .then((data) => {
+//     console.log("data is saved");
+//   })
+//   .catch((e) => {
+//     console.log("Failed: ", e);
+//   });
 // database.ref("age").set(21);
 // database.ref("location/city").set("Portland");
 // database
@@ -50,11 +128,11 @@ database
 //     console.log("failure: ", error);
 //   });
 
-database.ref().update({
-  "job/company": "Amazon",
-  "location/city": "Seattle",
-  stressLevel: 9,
-});
+// database.ref().update({
+//   "job/company": "Amazon",
+//   "location/city": "Seattle",
+//   stressLevel: 9,
+// });
 // database
 //   .ref("isSingle")
 //   .remove(() => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
+
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
   return {
-    entry: './src/app.js',
+    entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js',
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js",
     },
     module: {
       rules: [
         {
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/env', '@babel/react'],
-              plugins: [
-                '@babel/plugin-proposal-class-properties',
-                '@babel/plugin-transform-modules-commonjs',
-              ],
+              presets: ["@babel/env", "@babel/react"],
+              plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-transform-modules-commonjs"],
             },
           },
           test: /\.js$/,
@@ -31,13 +37,13 @@ module.exports = (env) => {
           use: CSSExtract.extract({
             use: [
               {
-                loader: 'css-loader',
+                loader: "css-loader",
                 options: {
                   sourceMap: true,
                 },
               },
               {
-                loader: 'sass-loader',
+                loader: "sass-loader",
                 options: {
                   sourceMap: true,
                 },
@@ -47,12 +53,23 @@ module.exports = (env) => {
         },
       ],
     },
-    plugins: [CSSExtract],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    plugins: [
+      CSSExtract,
+      new webpack.DefinePlugin({
+        "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
+      }),
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-      contentBase: path.join(__dirname, 'public'),
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      publicPath: '/dist/'
+      publicPath: "/dist/",
     },
   };
 };
